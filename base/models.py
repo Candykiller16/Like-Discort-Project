61787bd54a7bad14a7aabb250b579dc01a1062a4
Candykiller16 <Candykiller16@mail.ru>
2022-01-27 23:13:44 +0300

    fifth commit

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth .models import User
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Topic(models.Model):
@@ -8,12 +9,14 @@ class Topic(models.Model):
     def __str__(self):
         return self.name
 
+
 class Room(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) # автор комнаты
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True) # тема комнаты
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # автор комнаты
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)  # тема комнаты
     name = models.CharField(max_length=200)  # имя комнаты
     description = models.TextField(null=True, blank=True)  # описание о комнате, может быть пустое
-    participants = models.ManyToManyField(User, related_name='participants', blank=True)# хранит данные о юзерах, которые находятся в данный момент в комнате
+    participants = models.ManyToManyField(User, related_name='participants',
+                                          blank=True)  # хранит данные о юзерах, которые находятся в данный момент в комнате
     updated = models.DateField(auto_now=True)  # auto_now срабатывает каждый раз, когда мы изменяем запись в таблице
     created = models.DateField(auto_now_add=True)  # auto_now_add - работает 1 раз при создании записи в таблице
 
@@ -28,9 +31,11 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
-    update = models.DateField(auto_now=True)
+    updated = models.DateField(auto_now=True)
     created = models.DateField(auto_now_add=True)
 
-    def __str__(self):
-        return self.body[0:50] # в админке будет видеть с 0 по 49 символы поля body
+    class Meta:
+        ordering = ['-updated', '-created']
 
+    def __str__(self):
+        return self.body[0:50]  # в админке будет видеть с 0 по 49 символы поля body
